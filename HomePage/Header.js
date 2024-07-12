26842d665f0a053cae8b2bebe0c1cd68fdf308d0
emmanuellaAk <emmanuellaanimwaa17@icloud.com>
2024-07-12 15:43:20 +0000

    Revert "sidebar"
    This reverts commit 0857d3fcb6005a9002993123bedac09786cf2dd7.

--- HomePage/Header.js
+++ HomePage/Header.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
-import { StyleSheet, View, Image, TouchableOpacity, Modal, Text, ScrollView } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Image, TouchableOpacity} from "react-native";
+import sidebar from  
+import { useNavigation } from "@react-navigation/native";
 
+<<<<<<< HEAD
 export default function Header({ navigation }) {
   const [sidebarVisible, setSidebarVisible] = useState(false);
 
@@ -117,3 +120,77 @@ const styles = StyleSheet.create({
     marginVertical: 20,
   },
 });
+=======
+export default function Header(){
+    const navigation = useNavigation();
+
+    return(
+    <View style={styles.container}>
+        <View>
+            <Image style={styles.headermenu } source={require('../assets/Menu.png')}></Image>
+        </View>
+        <View>
+
+        </View>
+        <View>
+            <Image style={styles.headerlogo} source={require('../assets/Logo.png')}></Image>
+        </View>
+
+        <View style={styles.icons}>
+            <Image style={styles.searchIcon} source={require('../assets/Search-2.png')}></Image>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Cart')}>
+            <Image style={styles.shoppingBag} source={require('../assets/shoppingBag.png')}></Image>
+            </TouchableOpacity>
+        </View>
+    </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingTop: 60,
+        paddingBottom: 10,
+        flexDirection: 'row',
+        borderColor: 'black',
+        width: '100%',
+    },
+
+    headermenu: {
+        width: 30,
+        height:33
+    },
+
+    headerlogo:{
+        width: 99,
+        height: 40,
+        left: 21
+    },
+    
+    icons: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    searchIcon:{
+        width: 30,
+        height: 30, 
+        marginRight: 20
+    },
+
+    shoppingBag: {
+        width: 30,
+        height: 30
+    },
+    
+    button: {
+        position: 'relative',
+    }
+})
+
+
+
+>>>>>>> parent of 0857d3f (sidebar)
